34fb27dba9d6d948d09a525461e5a23932fa9bb5
George <gbh218@gmail.com>
2022-08-22 18:12:32 -0400

    Initial commit

---
+++ contracts/POD.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract POD is ERC721, Ownable {
+	uint256 private _supply;
+	uint256 private _maxSupply;
+	string private _metadataURI;
+	mapping(string => bool) private _claimed;
+
+	constructor(
+		string memory name,
+		string memory symbol,
+		address owner,
+		uint256 maxSupply,
+		string memory baseURI
+	) ERC721(name, symbol) {
+		_metadataURI = baseURI;
+		_maxSupply = maxSupply;
+		transferOwnership(owner);
+	}
+
+	function mint(address to, string memory claimCode) external onlyOwner {
+		require(!_claimed[claimCode], "POD: code has been claimed");
+		require(_supply < _maxSupply, "POD: max supply has been reached");
+		require(balanceOf(to) == 0, "POD: recipient balance greater than 0");
+		uint256 tokenId = _supply + 1;
+		_claimed[claimCode] = true;
+		_safeMint(to, tokenId);
+	}
+
+	function _baseURI() internal override view returns (string memory) {
+		return _metadataURI;
+	}
+}
---
+++ contracts/PODFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./POD.sol";
+
+contract PODFactory is Ownable {
+	event Deploy(
+		address contractAddress,
+		address creator,
+		address assignedTo
+	);
+
+	function create(
+		address assignedOwner,
+		uint256 maxSupply,
+		string memory baseURI
+	) public onlyOwner {
+		address newPODAddress = address(new POD("Proof of Drink", "POD", assignedOwner, maxSupply, baseURI));
+		emit Deploy(newPODAddress, msg.sender, assignedOwner);
+	}
+}
