6be0042bb09531f231b76e4640fc4c73c868545b
George <gbh218@gmail.com>
2022-08-24 23:23:37 -0400

    Replaced deploy script for factory contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const PODFactory = await hre.ethers.getContractFactory("PODFactory");
+  const factory = await PODFactory.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await factory.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `PODFactory deployed to ${factory.address}`,
+    `Ownership assigned to ${await factory.owner()}`
   );
 }
 
